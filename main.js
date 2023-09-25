892fa4aa371e27e08e42c5239d216f0cfdeb909e
DilawarBaig <dilawaralibaig@gmail.com>
2023-09-25 19:23:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+// Test 1
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test 2
+console.log("Is car != 'Subaru'? I predict false.");
+console.log(car != 'subaru');
+// Test 3
+console.log("Is car === 'mercedes'? I predict false.");
+console.log(car === 'mercedes');
+// Test 4
+console.log("Is car != 'saga'? I predict true.");
+console.log(car != 'saga');
+// Test 5
+console.log("Is car != 'Subaru'? I predict false.");
+console.log(car != 'subaru');
+// Test 6
+console.log("Is car === 'subaru'? I predict true.");
+console.log(car === 'subaru');
+// Test 7
+console.log("Is car != 'ford'? I predict true.");
+console.log(car != 'ford');
+// Test 8
+console.log("Is car === 'mazda'? I predict false.");
+console.log(car === 'mazda');
+// Test 9
+console.log("Is car === 'audi'? I predict false.");
+console.log(car === 'audi');
+// Test 10
+console.log("Is car != 'corolla'? I predict true.");
+console.log(car != 'corolla');
